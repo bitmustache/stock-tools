02d6ea2f1e2d8907d587d16c7769a89348df1acd
dmg4ming@gmail.com <dmg4ming@gmail.com>
2024-06-08 11:15:10 +0000

    Edited MainPage.CompoundInterestCalculator

--- client_code/MainPage/CompoundInterestCalculator/__init__.py
+++ client_code/MainPage/CompoundInterestCalculator/__init__.py
@@ -11,3 +11,21 @@ class CompoundInterestCalculator(CompoundInterestCalculatorTemplate):
 
   def mainpage_link_click(self, **event_args):
     open_form('MainPage')
+
+  def compoundinterestcalc_link_click(self, **event_args):
+    open_form('MainPage.CompoundInterestCalculator')
+
+  def calculate_compoundinterest_button_click(self, **event_args):
+    # get the user inputs
+    principal = float(self.principal_textbox.text)
+    interest_rate = float(self.interestrate_textbox.text)
+    years = float(self.years_textbox.text)
+    times_compounded = float(self.timescompounded_textbox.text)
+
+    # calculate the future value based on inputs
+    future_value = self.compound_interest(principal, interest_rate, years, times_compounded)
+
+    self.result_label.text = f"The future value of your investment is: ${future_value: .2f}"
+    
+  def compound_interest(self, principal, interest_rate, years, times_compounded):
+    amount = principal * (1 )
