05d08bea8bedeaa1e41c581fa10e4edcec29f2e6
dmg4ming@gmail.com <dmg4ming@gmail.com>
2024-06-28 11:14:38 +0000

    Edited MainPage and AdSense

--- client_code/AdSense/__init__.py
+++ client_code/AdSense/__init__.py
@@ -10,19 +10,18 @@ class AdSense(AdSenseTemplate):
 
     # Any code you write here will run before the form opens.
 
-  ad_code = '''
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4787864388418651"
-  crossorigin="anonymous"></script>
-  <!-- Horizontal Responsive -->
-  <ins class="adsbygoogle"
-      style="display:block"
-      data-ad-client="ca-pub-4787864388418651"
-      data-ad-slot="1877386277"
-      data-ad-format="auto"
-      data-full-width-responsive="true"></ins>
-  <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-  '''
-
-self.html_ad_container.html = ad_code
+  def display_ad(self):
+    ad_code = '''
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4787864388418651"
+    crossorigin="anonymous"></script>
+    <!-- Horizontal Responsive -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-4787864388418651"
+        data-ad-slot="1877386277"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    '''
--- client_code/MainPage/__init__.py
+++ client_code/MainPage/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import MainPageTemplate
 from anvil import *
 import anvil.server
+from ..AdSense import AdSense
 
 
 class MainPage(MainPageTemplate):
@@ -10,14 +11,23 @@ class MainPage(MainPageTemplate):
 
     # Any code you write here will run before the form opens.
 
+    # Initialize an instance of AdSense form
+    self.adsense_form = AdSense()
+
+    # Add the AdSense form to the ad_container component
+    self.ad_container.clear()
+    self.ad_container.add_component(self.adsense_form)
+
+    # Call the display_ad method to show the ad
+    self.adsense_form.display_ad()
+    
   def mainpage_link_click(self, **event_args):
     open_form('MainPage')
   
   def compoundinterestcalc_link_click(self, **event_args):
     open_form('MainPage.CompoundInterestCalculator')
 
-    self.ad_container.clear()
-    self.ad_container.add_component(AdSense())
+
   
 
 
